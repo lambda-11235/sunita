5afc731d1db1d662374f054357d302557c1979b0
Taran Lynn <taranlynn0@gmail.com>
2017-03-21 14:32:30 -0700

    Added derived SI units.

--- src/main/scala/sunita/SIUnitSystem.scala
+++ src/main/scala/sunita/SIUnitSystem.scala
@@ -3,6 +3,7 @@ package sunita
 
 
 object SIUnitSystem extends BaseUnitSystem {
+  // Base units
   val meter, m = newBaseUnit("meter", "m")
   val kilogram, kg = newBaseUnit("kilogram", "kg")
   val second, s = newBaseUnit("second", "s")
@@ -11,8 +12,24 @@ object SIUnitSystem extends BaseUnitSystem {
   val mole, mol = newBaseUnit("mole", "mol")
   val candela, cd = newBaseUnit("candela", "cd")
 
-  val hertz, Hz = zeroUnit/s
+  // Derived units
+  val hertz, Hz = zeroUnit / s
   val newton, N = m * kg / s.pow(2)
   val pascal, Pa = N / m.pow(2)
-  // TODO: Add all the named derived units
+  val joule, J = N * m
+  val watt, W = J / s
+  val coulomb, C = s * A
+  val volt, V = W / A
+  val farad, F = C / V
+  val ohm, Ω = V / A
+  val siemens, S = A / V
+  val weber, Wb = V * s
+  val tesla, T = Wb / m.pow(2)
+  val henry, H = Wb / A
+  val lumen, lm = cd
+  val lux, lx = lm / m.pow(2)
+  val becquerel, Bq = zeroUnit / s
+  val gray, Gy = J / kg
+  val sievert, Sv = J / kg
+  val katal, kat = mol / s
 }
